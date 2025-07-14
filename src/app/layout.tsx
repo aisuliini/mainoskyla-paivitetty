1c9e001cbd91a4e3f38d8b459f5fcb7555ad3d02
Ayca Belet <aycabelet@gmail.com>
2025-07-14 16:04:03 +0300

    ”muutokset”

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { Rubik } from 'next/font/google'
 import CookieBanner from '@/components/CookieBanner'
 import { Analytics } from '@vercel/analytics/react'
 import { AuthProvider } from '@/context/AuthContext'
