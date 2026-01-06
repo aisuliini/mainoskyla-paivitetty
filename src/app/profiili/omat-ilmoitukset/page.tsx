560a942a8119cb4e06bcc36c0c908f087057fdf0
Ayca Belet <aycabelet@gmail.com>
2026-01-06 11:59:28 +0200

    ”muutokset”

--- src/app/profiili/omat-ilmoitukset/page.tsx
+++ src/app/profiili/omat-ilmoitukset/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link'
 import Image from 'next/image'
 import { supabase } from '@/lib/supabaseClient'
 import { Eye } from 'lucide-react'
